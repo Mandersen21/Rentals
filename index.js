2e362d6d1ec29f093cb9892fdfd358828bd561f9
mandersen21 <mikkelandersen21@gmail.com>
2018-07-20 00:48:08 +0200

    Added code.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const Joi = require('joi');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded( { extended: true } ));
+app.use(express.static('public'));
+app.use(helmet()); 
+app.use(morgan('tiny')); // Logging
 
 const genres = [
     { id: 1, type: 'Action' },
