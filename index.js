ab13ea511061be2b708d50c6ea582edfb9d52f1a
mandersen21 <mikkelandersen21@gmail.com>
2018-07-24 12:28:48 +0200

    update.

--- index.js
+++ index.js
@@ -16,11 +16,5 @@ app.use(helmet());
 app.use('/', home);
 app.use('/api/genres', genres);
 
-// TODO: create a if statement only load in development mode
-app.use(morgan('tiny')); // Logging
-
-// Configuration
-console.log("Application name: " + config.get('name'))
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Listeling on port ' + port))
